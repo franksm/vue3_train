ba5372ba2c008c68e19ce9f7c5556ef03ceaa139
franksm <franksmhuang@gmail.com>
2023-07-06 14:49:02 +0800

    修正v-model:

--- src/views/course/prop/PropView.vue
+++ src/views/course/prop/PropView.vue
@@ -5,7 +5,7 @@
     2. 請讓顯示資訊能顯示現在密碼調整後的值
     <br />
     3. 請調用子組建的方法gohome
-    <emp-basic-form ref="empBasicFormRef" v-model:password="form.password" :name="form.name">
+    <emp-basic-form ref="empBasicFormRef" @password="form.password" :name="form.name">
     </emp-basic-form>
     <el-button @click="showInfo">顯示資訊</el-button>
     <el-button v-if="empBasicFormRef" @click="goHomeView">子組件的GOHOME</el-button>
