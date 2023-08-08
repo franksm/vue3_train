a78760ee2039b7bb76abfa8070a71bdfd1516144
franksm <franksmhuang@gmail.com>
2023-08-08 16:01:52 +0800

    簡化loading store操作

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,6 +1,5 @@
-import { ref, computed, type Ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import { statSync } from 'fs'
 
 let enableLoadingTime = 0;
 
@@ -18,46 +17,6 @@ export const useCounterStore = defineStore('counter', () => {
 
 export const useLoadingStore = defineStore('loading', () => {
   const isLoading = ref(false)
-  const loadingCounter = ref(0)
-
-  const enableLoading = () => {
-    enableLoadingTime = Date.now();
-    isLoading.value = true;
-  }
-
-  const disableLoading = () => {
-    // 最短loading時間，避免loading太快，畫面閃動
-    const minLoadingTime = 400;
-    const postLoadingTime = Date.now() - enableLoadingTime;
-    // 是否低於最短loading時間
-    const isShorterMin = postLoadingTime < minLoadingTime;
-    // 剩餘loading時間
-    const remainTime = minLoadingTime - postLoadingTime;
-
-    setTimeout(
-      () => {
-        if (loadingCounter.value <= 0 && isLoading.value) {
-          isLoading.value = false;
-        }
-      },
-      isShorterMin ? remainTime : 0
-    );
-    
-  }
-
-  const incrementLoadingCounter = () => {
-    loadingCounter.value += 1;
-    if (loadingCounter.value > 0 && !isLoading.value) {
-      enableLoading();
-    }
-  }
-
-  const decrementLoadingCounter = () => {
-    loadingCounter.value -= 1;
-    if (loadingCounter.value <= 0 && isLoading.value) {
-      disableLoading();
-    }
-  }
 
   const openLoading = () => {
     if (!isLoading.value) {
